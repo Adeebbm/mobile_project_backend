6ed9f4e1ce634d8e0f5666bbe3c10b434afa5124
Hadi-001 <hadi.shayya01@lau.edu>
2025-04-28 03:18:10 +0300

    Add get_all_estates.php

--- get_all_estates.php
+++ get_all_estates.php
@@ -1,9 +1,26 @@
 <?php
   include 'connection.php';
   header('Content-Type: application/json');
+  
+  // Query to fetch all estates
   $res = mysqli_query($conn, "SELECT * FROM estate");
+  
   $out = [];
+  
   while($row = mysqli_fetch_assoc($res)) {
+    $row['estate_id'] = (int) $row['estate_id'];
+    $row['beds'] = (int) $row['beds'];
+    $row['baths'] = (int) $row['baths'];
+    $row['price'] = (float) $row['price']; 
+    $row['views'] = (int) $row['views'];
+    $row['area'] = (float) $row['area'];
+    $row['owner_id'] = (int) $row['owner_id'];
+    $row['date_build'] = $row['date_built'];  
+
+    unset($row['date_built']);
+    
     $out[] = $row;
   }
+  
   echo json_encode($out);
+?>
